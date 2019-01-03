0acf790bb80bdcd75f5ef61b2ae466b3807a2974
gdr43 <dilip.gujju@gmail.com>
2019-01-03 21:35:31 +0530

    All commits

---
+++ tableofmovies/src/components/Home/Home.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import { getMovies } from "../../services/fakeMovieService";
+
+class Home extends Component {
+  state = {
+    movies: getMovies()
+  };
+
+  handleDelete = movie => {
+    const movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({ movies });
+  };
+
+  render() {
+    const { movies } = this.state;
+    if (movies.length === 0) return <p>There are no movies in the database.</p>;
+    return (
+      <div>
+        <p>Showing {movies.length} movies in the list.</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {movies.map(movie => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={this.handleDelete.bind(this, movie)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Home;
