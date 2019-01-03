eae4ee069b358d4d3f96b9525afbd69de714c469
gdr43 <dilip.gujju@gmail.com>
2019-01-03 22:01:24 +0530

    Created counter component

--- cart-counter/src/components/Counter.jsx
+++ cart-counter/src/components/Counter.jsx
@@ -1,10 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-    state = {  }
-    render() { 
-        return (  );
-    }
+  render() {
+    return (
+      <div>
+        <span className="badge badge-primary p-2">Zero</span>
+        <button className="btn btn-secondary m-2">Increment</button>
+        <button className="btn btn-danger m-2">Delete</button>
+      </div>
+    );
+  }
 }
- 
+
 export default Counter;
